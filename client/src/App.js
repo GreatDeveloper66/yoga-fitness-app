0aabf95275dd6f8931442166c3e997a5a5a59da8
GreatDeveloper66 <ashaffe@gmail.com>
2025-01-24 19:58:32 -0600

    Add Home Screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { AppContext, useAppContext } from './AppContext/AppContext';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <div className="App">
       <AppContext.Provider value={useAppContext()}>
-      <p>App</p>
+        <Router>
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
       </AppContext.Provider>
     </div>
 
